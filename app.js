fc6bac8572294cd2ef5a505af0e6d336f3c19261
fuhuan1991 <huanfu2019@gmail.com>
2021-05-18 11:29:35 -0400

    clean

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/public'))
-
 
 var server = app.listen(7777, () => {
     console.log("Listening on port " + server.address().port + "...");
